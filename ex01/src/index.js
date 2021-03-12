7b0028c590bb94b883f3e642de3a6857bdbe16b1
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 00:40:05 +0100

    Added first index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,20 @@
+function main() {
+
+    // Only change code below this line
+    
+    var myFirstString = "I think therefore I \"am\" -- sentient.";
+    var mySecondString = "No, you only \"think\", so you aren't.";
+
+    // Only change code abowe this line
+    return {
+
+        myFirstString,
+        mySecondString
+
+    };
+
+
+}
+
+console.log(main());
+module.exports = main;
