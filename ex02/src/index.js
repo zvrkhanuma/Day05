7610dde649924d2a0648078e63692235bae2700c
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 00:57:39 +0100

    Added second index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+	// Only change code below this line
+
+	var myLink = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+	// Only change code above this line
+	return myLink;
+}
+
+console.log(main());
+module.exports = main;
