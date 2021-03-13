bbd70ba896fafd2b64ece58a26892a128f1ee224
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 09:52:21 +0100

    Modified index.js

---
+++ ex03/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+	// Only change code below this line
+
+	var myString = "FirstLine\n\t\\SecondLine\nThirdLine";
+
+	// Only change code above this line
+	return myString;
+}
+
+console.log(main());
+module.exports = main;
