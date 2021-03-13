8171407a052e5883ae9a6f05b62aadc60623d2d3
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 10:19:08 +0100

    Added thirteenth index.js

---
+++ ex13/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+	// Only change code below this line
+
+	var lastName = "Bezos";
+    var thirdToLastLetterOfLastName = lastName[lastName.length-3];
+
+	// Only change code above this line
+	return thirdToLastLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
