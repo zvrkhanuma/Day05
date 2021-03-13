62f7cab6d55d555c5899ba9a093dbeff1706ca81
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 10:22:32 +0100

    Added fourteenth index.js

---
+++ ex14/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+	// Only change code below this line
+
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks = "My "+ myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; 
+
+	// Only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
