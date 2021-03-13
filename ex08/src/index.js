f7873d7e6e9193d896213d349c3612bf89d4bb89
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 10:02:50 +0100

    Added eight index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+	// Only change code below this line
+
+	var lastName = "Doe";
+	var lastNameLength = lastName.length;
+
+	// Only change code above this line
+	return lastNameLength;
+}
+
+console.log(main());
+module.exports = main;
