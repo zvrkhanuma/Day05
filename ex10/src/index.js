a0cdaab4655f891b8d0d83da55cfc16daf952b57
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 10:07:19 +0100

    Added tenth index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+	// Only change code below this line
+
+	var myString = "Jello World";
+	myString = "Hello World";
+
+	// Only change code above this line
+	return myString;
+}
+
+console.log(main());
+module.exports = main;
