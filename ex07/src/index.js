597bd8c1bcbe92a28efa11939e86f8779ccc6ab3
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 10:00:48 +0100

    Added seventh index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+	// Only change code below this line
+
+	var someAdjective = "the best!";
+	var myString = "Mistral is ";
+	myString += someAdjective;
+
+	// Only change code above this line
+	return myString;
+}
+
+console.log(main());
+module.exports = main;
