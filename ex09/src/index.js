183d7e5874c47bbce14cd666e643b89c06565147
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 10:05:08 +0100

    Added nineth index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+	// Only change code below this line
+
+	var lastName = "Doe";
+	var firstLetterOfLastName = " ";
+	firstLetterOfLastName = lastName[0];
+
+	// Only change code above this line
+	return firstLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
