0f8977d18348c7bcdc32d83faab95a7b2f4096c5
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 09:58:35 +0100

    Added sixth index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+	// Only change code below this line
+
+	var myName = "Alma";
+	var myString = "My name is " + myName + " and I am well!";
+
+	// Only change code above this line
+	return myString;
+}
+
+console.log(main());
+module.exports = main;
