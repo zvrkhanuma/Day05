639d2e6bfa4dbb7fd95f60b818100c296f357992
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 10:14:35 +0100

    Added twelfth index.js

---
+++ ex12/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+	// Only change code below this line
+
+	var lastName = "Tesla";
+    var lastLetterOfLastName = lastName[lastName.length-1];
+
+	// Only change code above this line
+	return lastLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
