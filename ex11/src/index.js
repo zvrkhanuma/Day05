3912162e4dbc92348c258026336f8a34fbc97d9e
zvrkhanuma <almazalovic@gmail.com>
2021-03-13 10:12:09 +0100

    Added eleventh index.js

---
+++ ex11/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+	// Only change code below this line
+
+	var lastName = "Tesla";
+    var thirdLetterOfLastName = lastName[2];
+
+	// Only change code above this line
+	return thirdLetterOfLastName;
+}
+
+console.log(main());
+module.exports = main;
